82fed897e156bdd6257febaac6d58816aa0b03d0
syf-dev <syfdex9994@gmail.com>
2025-07-24 18:59:43 +0530

    Improved Start/Restart button interaction

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function onStartButton() {
+function toggleStartButton() {
     const startButton = document.getElementById('start-button');
-    startButton.textContent = 'RESTART';
+    const restartButtonArea = document.getElementById('restart-button-area');
+
+    var toggledButton = startButton.textContent=="▶ START" ? "● STOP"  : "▶ START";
+    startButton.textContent = toggledButton;
+
+    restartButtonArea.innerHTML = '<button id="restart-button">⟳ RESTART</button>';
 };
