ec1be4a56c043c870ea29d0dbb247fa5db418423
syf-dev <syfdex9994@gmail.com>
2025-07-28 16:14:07 +0530

    Added Food placement and eating logic

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
 const playButtonArea = document.getElementById('play-button-area');
 const restartButtonArea = document.getElementById('restart-button-area');
 const gameCanvas = document.getElementById('game-canvas');
+const gameScore = document.getElementById('game-score');
 const gameBox = document.querySelector('.game-box');
 
 let snakeDirection = 0;
 let snakeBody = [
     {x: 25, y: 25}
 ];
+let speedInterval = 100;
+let foodPosition = {x: 0, y: 0}
+let score = 0;
 
 const ctx = gameCanvas.getContext('2d');
 const segmentSize = 20;
 
-function getSnake() {
+function drawGame() {
     ctx.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
-    
+
     snakeBody.forEach(segment => {
         ctx.lineWidth = 3;
-        ctx.strokeStyle = 'rgb(102, 77, 32)';
-        ctx.fillStyle = 'rgb(160, 116, 5)';
+        ctx.strokeStyle = 'rgba(83, 102, 32, 1)';
+        ctx.fillStyle = 'rgba(131, 160, 5, 1)';
         ctx.beginPath();
         ctx.roundRect(segment.x, segment.y, segmentSize, segmentSize, 3);
         ctx.fill();
         ctx.stroke();
     });
+
+    ctx.fillStyle = 'rgba(156, 51, 25, 1)';
+    ctx.beginPath();
+    ctx.roundRect(foodPosition.x, foodPosition.y, 15, 15, 10);
+    ctx.fill();
+}
+
+function placeFood() {
+    do {
+        foodPosition.x = Math.floor((Math.random() * (gameCanvas.width - segmentSize)) / segmentSize) * segmentSize + 5;
+        foodPosition.y = Math.floor((Math.random() * (gameCanvas.height - segmentSize)) / segmentSize) * segmentSize + 5;
+    } while (snakeBody.some(segment => segment.x === foodPosition.x - 5 && segment.y === foodPosition.y - 5));
+    drawGame();
 }
 
 function togglePlayButton(pause, hide) {
@@ -31,6 +48,7 @@ function togglePlayButton(pause, hide) {
     } else {
         moveSnake();
         playButtonArea.innerHTML = '<button id="play-button" onclick="togglePlayButton(pause=true)">● PAUSE</button>';
+        gameBox.querySelector('.game-message').style.border = 'none'
         gameBox.querySelector('.game-message').textContent = '';
     }
 
@@ -42,17 +60,14 @@ function togglePlayButton(pause, hide) {
 function setCanvasStyle(out) {
     if (out) {
         gameCanvas.style.backgroundColor = 'rgba(24, 9, 9, 1)';
-        gameBox.style.border = 'dashed 1px rgb(196, 97, 97)';
-        gameBox.querySelector('.score-container').style.backgroundColor = 'rgba(143, 55, 55, 1)';
-        gameBox.querySelector('.score-container').style.borderBottom = 'solid 1px rgb(196, 97, 97)';
-        gameBox.querySelector('.game-message').style.margin = '180px 0px 0px 68px';
+        gameBox.querySelector('.game-message').style.border = '';
+        gameBox.querySelector('.game-message').style.margin = '180px 0px 0px 80px';
         gameBox.querySelector('.game-message').style.color = 'rgb(77, 27, 18)';
-        gameBox.querySelector('.game-message').textContent = 'GAME OVER!';
+        gameBox.querySelector('.game-message').textContent = 'GAME OVER';
         restartButtonArea.innerHTML = '<button id="restart-button" onclick="restartGame()">⟳ RESTART</button>';
     } else {
+        gameScore.innerText = score;
         gameCanvas.style = '';
-        gameBox.style = '';
-        gameBox.querySelector('.score-container').style = '';
         gameBox.querySelector('.game-message').style = '';
         gameBox.querySelector('.game-message').textContent = '';
         restartButtonArea.innerHTML = '';
@@ -64,9 +79,13 @@ function restartGame() {
     snakeBody = [
         {x: 25, y: 25}
     ];
+    speedInterval = 100;
+    foodPosition = {x: 0, y: 0}
+    score = 0;
 
     setCanvasStyle(out=false);
     togglePlayButton(pause=false, hide=false);
+    placeFood();
 }
 
 function gameOver() {
@@ -97,16 +116,27 @@ function moveSnake() {
             if (snakeBody.some(segment => segment.x === head.x && segment.y === head.y)) { 
                 gameOver();
                 clearInterval(movementInterval);
+                return;
+            }
+
+            if (head.x === foodPosition.x && head.y === foodPosition.y) {
+                score += 10;
+                gameScore.innerText = score;
+                snakeBody.push({x: head.x, y: head.y});
+                placeFood();
+                speedInterval -= 5;
+                clearInterval(movementInterval);
+                moveSnake();
             }
 
             snakeBody.unshift(head);
             snakeBody.pop();
 
-            getSnake();
+            drawGame();
         } else {
             clearInterval(movementInterval);
         }
-    }, 100);
+    }, speedInterval);
 }
 
 window.addEventListener('keyup', (e) => {
